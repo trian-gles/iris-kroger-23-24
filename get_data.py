bab20d2f89fab470b40d7be9c264c7c53e76a9a6
Kieran McAuliffe <69212477+trian-gles@users.noreply.github.com>
2024-02-25 13:29:19 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -31,9 +31,9 @@ st7735.begin()
 class Temp:
     def __init__():
         # Tuning factor for compensation. Decrease this number to adjust the
-    # temperature down, and increase to adjust up
-    self.factor = 2.25
-    self.cpu_temps = [get_cpu_temperature()] * 5
+        # temperature down, and increase to adjust up
+        self.factor = 2.25
+        self.cpu_temps = [get_cpu_temperature()] * 5
         
 # Get the temperature of the CPU for compensation
     def get_cpu_temperature(self):
