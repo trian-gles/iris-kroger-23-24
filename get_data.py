315039ddab4add8561fb7f20a4e71988afb2e323
Kieran McAuliffe <69212477+trian-gles@users.noreply.github.com>
2024-02-25 13:46:03 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -2,14 +2,7 @@ import time
 import os
 import sys
 import ST7735
-try:
-    # Transitional fix for breaking change in LTR559
-    from ltr559 import LTR559
-    ltr559 = LTR559()
-except ImportError:
-    import ltr559
 
-from bme280 import BME280
 from enviroplus import gas
 from subprocess import PIPE, Popen
 
