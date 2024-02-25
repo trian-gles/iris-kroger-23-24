01b734340b9e1e89392d052d5f0a1007e3489dc4
Kieran McAuliffe <69212477+trian-gles@users.noreply.github.com>
2024-02-25 13:20:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import azure_contact
-#import sensor_data
+import get_data
 import asyncio
 import json
+import logging
+import time
+
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
 
 async def main():
 	d = open('device.json')
@@ -10,6 +18,13 @@ async def main():
 
 	connection = azure_contact.Connection(conn_str)
 	await connection.connect()
+	last_send_time = time.time()
+	while True:
+		temp = get_data.get_temp()
+		logging.info($"Temp : {temp}")
+		while time.time() - last_send_time < 10:
+			pass
+		
 	await connection.send_message("Connected!")
 
 	await connection.close()
