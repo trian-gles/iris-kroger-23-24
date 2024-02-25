10a63cc4c84700918bb5f02e3830811dc41eef9e
Kieran McAuliffe <69212477+trian-gles@users.noreply.github.com>
2024-02-25 13:26:30 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -28,25 +28,27 @@ st7735 = ST7735.ST7735(
 # Initialize display
 st7735.begin()
 
+class Temp:
+    def __init__():
+        # Tuning factor for compensation. Decrease this number to adjust the
+    # temperature down, and increase to adjust up
+    self.factor = 2.25
+    self.cpu_temps = [get_cpu_temperature()] * 5
+        
 # Get the temperature of the CPU for compensation
-def get_cpu_temperature():
-    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
-    output, _error = process.communicate()
-    return float(output[output.index('=') + 1:output.rindex("'")])
+    def get_cpu_temperature(self):
+        process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE, universal_newlines=True)
+        output, _error = process.communicate()
+        return float(output[output.index('=') + 1:output.rindex("'")])
 
-# Tuning factor for compensation. Decrease this number to adjust the
-# temperature down, and increase to adjust up
-factor = 2.25
+    def get_temp(self):
+      cpu_temp = get_cpu_temperature()
+      # Smooth out with some averaging to decrease jitter
+      self.cpu_temps = self.cpu_temps[1:] + [cpu_temp]
+      avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+      raw_temp = bme280.get_temperature()
+      return raw_temp - ((avg_cpu_temp - raw_temp) / factor)
 
-cpu_temps = [get_cpu_temperature()] * 5
-
-def get_temp():
-  cpu_temp = get_cpu_temperature()
-  # Smooth out with some averaging to decrease jitter
-  cpu_temps = cpu_temps[1:] + [cpu_temp]
-  avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
-  raw_temp = bme280.get_temperature()
-  return raw_temp - ((avg_cpu_temp - raw_temp) / factor)
 
 def get_humidity():
   return bme280.get_humidity()
