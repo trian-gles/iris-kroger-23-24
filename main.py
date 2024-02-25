65eac6c6b93b705dbf1616dee4be9923d5d0c9a9
Kieran McAuliffe <69212477+trian-gles@users.noreply.github.com>
2024-02-25 13:33:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ async def main():
 
 	connection = azure_contact.Connection(conn_str)
 	await connection.connect()
-	last_send_time = time.time()
+	
 
 	temp_obj = get_data.Temp()
 	
 	while True:
 		temp = temp_obj.get_temp()
+		last_send_time = time.time()
 		logging.info(f"Temp : {temp}")
 		while time.time() - last_send_time < 10:
 			pass
