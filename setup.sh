4a20cba707ed67fc3aa87eecf087de40737fc880
Kieran McAuliffe <69212477+trian-gles@users.noreply.github.com>
2024-02-25 12:56:36 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 python -m venv env
 source /env/bin/activate
 pip install azure-iot-device
+sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
+sudo apt install git
+git clone https://github.com/pimoroni/enviroplus-python
+cd enviroplus-python
+sudo ./install.sh
