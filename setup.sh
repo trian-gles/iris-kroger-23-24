d3e9c0a05f126806a5b3b39b3f0d046137512236
Kieran McAuliffe <69212477+trian-gles@users.noreply.github.com>
2024-03-14 11:28:58 -0400

    fixed order in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 python -m venv env
 source /env/bin/activate
-pip install azure-iot-device
+
 sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 sudo apt install git
 git clone https://github.com/pimoroni/enviroplus-python
@@ -9,4 +9,5 @@ sudo ./install.sh
 cd ..
 pip install pms5003==0.0.5
 pip install st7735==0.0.5
+pip install azure-iot-device
 
